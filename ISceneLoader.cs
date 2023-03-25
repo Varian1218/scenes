b6471c3b3dbee6da32fe762cd6b1d35590ccae44
DESKTOP-UJRR6FS\Admin <tuduchoang@gmail.com>
2023-03-25 11:39:48 +0700

    1.0.1

---
+++ ISceneLoader.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace Scenes
+{
+    public interface ISceneLoader
+    {
+        IEnumerator<T> LoadSceneAsync<T>(string sceneName) where T : class;
+    }
+}
---
+++ SceneLoader.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using Object = UnityEngine.Object;
+
+namespace Scenes
+{
+    public class SceneLoader : ISceneLoader
+    {
+        private readonly Dictionary<string, Type> _types = new();
+
+        public void Add<TInterface, TClass>()
+        {
+            _types.Add(GetHash<TInterface>(), typeof(TClass));
+        }
+
+        private static string GetHash<T>()
+        {
+            return typeof(T).Name;
+        }
+
+        public IEnumerator<T> LoadSceneAsync<T>(string sceneName) where T : class
+        {
+            var asyncOperation = SceneManager.LoadSceneAsync(sceneName);
+            while (!asyncOperation.isDone)
+            {
+                yield return null;
+            }
+
+            yield return Object.FindObjectOfType(_types[GetHash<T>()]) as T;
+        }
+    }
+}

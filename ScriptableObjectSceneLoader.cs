0f7c1095c63a5db35a207d41fff8ad7108f6f067
DESKTOP-UJRR6FS\Admin <tuduchoang@gmail.com>
2023-04-02 16:01:58 +0700

    1.0.3

---
+++ ScriptableObjectSceneLoader.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Scenes
+{
+    public class ScriptableObjectSceneLoader : ScriptableObject, ISceneLoader
+    {
+        private ISceneLoader _impl;
+
+        public ISceneLoader Impl
+        {
+            set => _impl = value;
+        }
+
+        public IEnumerator<T> LoadSceneAsync<T>(string sceneName) where T : class
+        {
+            return _impl.LoadSceneAsync<T>(sceneName);
+        }
+    }
+}
